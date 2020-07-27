d1d795b41e840546cc3089dde916ceb4bd902829
Zachary Baum <zac_baum@hotmail.com>
2020-07-27 12:49:14 -0400

    Remove poor quality images before predicting on all folds

--- predict.py
+++ predict.py
@@ -1,9 +1,10 @@
-import tensorflow as tf
-import numpy as np
-import os
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.utils import to_categorical
+import cv2
+import numpy as np
+import os
+import tensorflow as tf
 
 from utils import PlotLosses, ConfusionMatrixPlotter
 
@@ -20,39 +21,51 @@ test_flow = test_datagen.flow_from_directory(
     directory=os.path.join(data_folder, "test"),
     target_size=(input_shape[0], input_shape[1]),
     color_mode="grayscale",
-    batch_size=batch_size,
+    batch_size=1,
     class_mode="binary",
     shuffle=False,
 )
 
-print("ASSESS QUALITY")
+x, y = zip(*(test_flow[i] for i in range(0, len(test_flow), 1)))
+x_val, y_val = np.vstack(x), np.vstack(to_categorical(y))[:, 1]
 
-for qual in ["quality-cls-50to1", "quality-cls-100to1"]:
+print("ASSESS QUALITY...\n")
 
-    model = load_model(qual + ".h5", compile=False)
+model = load_model("quality-cls-50to1.h5", compile=False)
+preds = model.predict(
+    x_val,
+    batch_size=256, 
+    max_queue_size=250, 
+    workers=32,
+)
+preds = np.round(np.squeeze(preds))
 
-    preds = model.predict(
-        test_flow, 
-        max_queue_size=250, 
-        workers=32,
-        verbose=1,
+x_good = np.array([x_val[i] for i, x in enumerate(preds) if x == 1])
+x_poor = [x_val[i] for i, x in enumerate(preds) if x == 0]
+
+for i, im in enumerate(x_poor):
+    cv2.imwrite(
+        os.path.join("./result-50to1-remove/" + str(i) + ".png"),
+        cv2.cvtColor(im, cv2.COLOR_GRAY2RGB)
     )
 
-    preds = np.round(preds)
-    print(sum(preds) / len(preds))
+print("\nPREDICT (+) vs (-) ON GOOD IMAGES")
 
-print("PREDICT (+) vs (-)")
 
 for fold in ["fold1", "fold2", "fold3", "fold4", "fold5"]:
+
+    print("\n===================================================")
+    print("                       " + fold + "                       ")
+    print("===================================================")
 
     model = load_model(fold + ".h5", compile=False)
 
     preds = model.predict(
-        test_flow, 
+        x_good,
+        batch_size=256, 
         max_queue_size=250, 
         workers=32,
-        verbose=1,
     )
 
-    preds = np.round(preds)
+    preds = np.round(np.squeeze(preds))
     print(sum(preds) / len(preds))
